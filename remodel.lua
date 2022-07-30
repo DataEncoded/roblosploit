6ec99b7f68a9684204d1d5ec9a27cd558b4f4a49
DataEncoded <dataencoded@gmail.com>
2022-07-30 12:44:47 -0500

    [chore] Add check to ensure folder isn't included in remodel and add serverscriptservice detection

--- remodel.lua
+++ remodel.lua
@@ -1,9 +1,30 @@
 local game = remodel.readPlaceFile("build.rbxlx")
 
 local shared = game.ReplicatedStorage.roblosploit_shared
+local server = game.ServerScriptService.roblosploit_server
 
 for _, v in ipairs(shared:GetChildren()) do
-    if not (v.ClassName == "Script" or v.ClassName == "LocalScript" or v.ClassName == "ModuleScript") then
-        remodel.writeModelFile(v, "src/shared/" .. v.Name .. ".rbxmx")
-    end
+	if
+		not (
+			v.ClassName == "Script"
+			or v.ClassName == "LocalScript"
+			or v.ClassName == "ModuleScript"
+			or v.ClassName == "Folder"
+		)
+	then
+		remodel.writeModelFile(v, "src/shared/" .. v.Name .. ".rbxmx")
+	end
+end
+
+for _, v in ipairs(server:GetChildren()) do
+	if
+		not (
+			v.ClassName == "Script"
+			or v.ClassName == "LocalScript"
+			or v.ClassName == "ModuleScript"
+			or v.ClassName == "Folder"
+		)
+	then
+		remodel.writeModelFile(v, "src/server/" .. v.Name .. ".rbxmx")
+	end
 end
